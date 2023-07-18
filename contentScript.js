4c26bb2e76e55c924bc728c6c9d170324e46813c
Jay Liew <jayliew@jayliew.com>
2023-07-18 16:27:52 -0400

    added TLDs with high likelihood of being used for phishing

--- contentScript.js
+++ contentScript.js
@@ -11,6 +11,33 @@ function checkIfHostnameHasSuspiciousTlds(hostnameStr){
   if(hostnameStr.endsWith('.mov') == true){
     alert("Caution: Hostname in URL ends with .mov");
   }
+  if(hostnameStr.endsWith('.cn') == true){
+    alert("Caution: Hostname in URL ends with .cn");
+  }
+  if(hostnameStr.endsWith('.ru') == true){
+    alert("Caution: Hostname in URL ends with .ru");
+  }
+  if(hostnameStr.endsWith('.in') == true){
+    alert("Caution: Hostname in URL ends with .in");
+  }
+  if(hostnameStr.endsWith('.xyz') == true){
+    alert("Caution: Hostname in URL ends with .xyz");
+  }
+  if(hostnameStr.endsWith('.me') == true){
+    alert("Caution: Hostname in URL ends with .me");
+  }
+  if(hostnameStr.endsWith('.br') == true){
+    alert("Caution: Hostname in URL ends with .br");
+  }
+  if(hostnameStr.endsWith('.page') == true){
+    alert("Caution: Hostname in URL ends with .page");
+  }
+  if(hostnameStr.endsWith('.icu') == true){
+    alert("Caution: Hostname in URL ends with .icu");
+  }
+  if(hostnameStr.endsWith('.ke') == true){
+    alert("Caution: Hostname in URL ends with .ke");
+  }
 }
 
 function parseHostnameFromUrl(windowLocationHref){
